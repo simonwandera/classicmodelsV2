a93d134ab43d0e730d65f3cb58a3db14c61e294c
MaryAnn-oooo <maryannwathera699@gmail.com>
2025-07-23 16:34:37 +0300

    Resolving conflicts

--- src/main/java/com/systech/systech/Entity/Product.java
+++ src/main/java/com/systech/systech/Entity/Product.java
@@ -1,6 +1,6 @@
-
 package com.systech.systech.Entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -9,11 +9,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
+
 
 @Entity
 @Table(name = "products")//I think the table name here is supposed to be products
@@ -27,30 +31,35 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(name = "productCode")
     private String productCode;
 
     @Column(nullable = false)
     private String productName;
 
     @Column(nullable = false)
-    private String productScale;
+    private String MSRP; // Manufacturer's Suggested Retail Price
 
-    @Column(nullable = false)
-    private String productVendor;
-
-    @Column(nullable = false)
-    private String productDescription;
-
-    @Column(nullable = false)
-    private Short quantityInStock;
-
-    @Column(nullable = false)
-    private String buyPrice;
-
-
-    @ManyToOne(fetch = FetchType.LAZY, optional = false )
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "product_line_id")
-    private ProductLine productLine;
+    private ProductLine product_line;
 
+    @Column(nullable = false)
+    private String product_scale;
+
+    @Column(nullable = false)
+    private String product_vendor;
+
+    @Column(nullable = false)
+    private String product_description;
+
+    @Column(nullable = false)
+    private String quantity_in_stock;
+
+    @Column(nullable = false)
+    private String buy_price;
+
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<OrderDetails> orderDetails;
 }
