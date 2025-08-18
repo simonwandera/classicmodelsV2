2bcb3192a829316a8da26f083d933ddca3e698aa
FianKisero <kiserofian11@gmail.com>
2025-08-18 21:15:05 +0300

    PRODUCTLINE PAGE IMPL

--- src/main/java/com/systech/systech/controller/ProductLineController.java
+++ src/main/java/com/systech/systech/controller/ProductLineController.java
@@ -1,50 +1,65 @@
 package com.systech.systech.controller;
 
 import com.systech.systech.Entity.ProductLine;
+import com.systech.systech.service.FileStorageService;
 import com.systech.systech.service.ProductLineService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/productLine")
+@RequestMapping("api/product-lines")
 @RequiredArgsConstructor
 @Slf4j
+
 public class ProductLineController {
 
+
     private final ProductLineService productLineService;
-
-    //     Example method to get all products
+    private final FileStorageService fileStorageService;
+    // GET all
     @GetMapping
-    public ResponseEntity<List<ProductLine>> getList() {
-        List<ProductLine> list = productLineService.getAllProductLines();
-
-//         ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
-        return ResponseEntity.ok(list);
+    public ResponseEntity<List<ProductLine>> getAll() {
+        return ResponseEntity.ok(productLineService.getAllProductLines());
     }
 
-    @PostMapping("/createOrUpdate")
-    public ResponseEntity<ProductLine> createProductLine(@RequestBody ProductLine product) {
+    // GET by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductLine> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(productLineService.getProductLineById(id));
+    }
 
-        log.info("Creating or updating product line: {}", product);
-        try {
-            productLineService.createProductLine(product);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(null);
+    //CREATE
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ProductLine> create(
+            @RequestPart("data") ProductLine productLine,
+            @RequestPart(value = "image", required = false) MultipartFile imageFile) {
+
+        if (imageFile != null) {
+            productLine.setImage(fileStorageService.storeFile(imageFile));
         }
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(product);
+        return ResponseEntity.ok(productLineService.createProductLine(productLine));
     }
 
+    // UPDATE
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductLine> update(@PathVariable Long id,
+                                              @RequestBody ProductLine productLine) {
+        return ResponseEntity.ok(productLineService.updateProductLine(id, productLine));
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        productLineService.deleteProductLine(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
